ba5467f88dddb4c39ad5b7b2eb3417ee6ae16861
Ivan <fedivmi@gmail.com>
2022-01-29 22:08:08 +0500

    added TouchID for notes

--- app/src/main/java/com/example/news_app/Fragments/NotesSecurityDialogFragment.kt
+++ app/src/main/java/com/example/news_app/Fragments/NotesSecurityDialogFragment.kt
@@ -1,12 +1,20 @@
 package com.example.news_app.Fragments
 
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.DialogFragment
 import com.example.news_app.Models.NewsHeadlines
 import com.example.news_app.R
@@ -32,6 +40,25 @@ class NotesSecurityDialogFragment(var headlines: NewsHeadlines) : DialogFragment
 
     private var password: String? = null
 
+    private var cancellationSignal: CancellationSignal? = null
+    private val authenticationCallback: BiometricPrompt.AuthenticationCallback
+        get() = @RequiresApi(Build.VERSION_CODES.P)
+        object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                super.onAuthenticationError(errorCode, errString)
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                println("Touchid Success")
+                if(context != null){
+                    Toast.makeText(context, "Authentication success", Toast.LENGTH_SHORT).show()
+                }
+                openNotes()
+                dismiss()
+                super.onAuthenticationSucceeded(result)
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -48,6 +75,13 @@ class NotesSecurityDialogFragment(var headlines: NewsHeadlines) : DialogFragment
 
         btn_touch_id.setOnClickListener {
             println("TouchId")
+            if(checkBiometricSupport() && Build.VERSION.SDK_INT > Build.VERSION_CODES.P){
+                val biometricPrompt = BiometricPrompt.Builder(requireContext())
+                    .setTitle("Notes authentication")
+                    .setNegativeButton("Cancel", requireContext().mainExecutor){_,_->}
+                    .build()
+                biometricPrompt.authenticate(getCancellationSignal(), requireContext().mainExecutor, authenticationCallback)
+            }
         }
 
         btn_confirm.setOnClickListener {
@@ -57,7 +91,7 @@ class NotesSecurityDialogFragment(var headlines: NewsHeadlines) : DialogFragment
                 dismiss()
                 openNotes()
             } else {
-                Toast.makeText(requireContext(), "Fail", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "Wrong password", Toast.LENGTH_SHORT).show()
             }
             et_password.text = null
         }
@@ -101,5 +135,52 @@ class NotesSecurityDialogFragment(var headlines: NewsHeadlines) : DialogFragment
             userBookmarksReference = currentUserReference.child("bookmarks")
             userStatsReference = currentUserReference.child("stats")
         }
+    }
+
+    private fun checkBiometricSupport(): Boolean {
+        try {
+            val keyguardManager: KeyguardManager =
+                requireContext().getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            if (!keyguardManager.isKeyguardSecure) {
+                Toast.makeText(
+                    context,
+                    "Fingerprint authentication is not enabled",
+                    Toast.LENGTH_SHORT
+                ).show()
+                return false
+            }
+
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    android.Manifest.permission.USE_BIOMETRIC
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(
+                    context,
+                    "Fingerprint authentication permission is not enabled",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                return false
+            }
+
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requireContext().packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)
+            } else {
+                false
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
+    }
+
+    private fun getCancellationSignal(): CancellationSignal{
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            Toast.makeText(context, "Authentication was cancelled", Toast.LENGTH_SHORT).show()
+        }
+        return cancellationSignal as CancellationSignal
     }
 }
